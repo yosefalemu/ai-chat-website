9aec72c965be9085408eb5802444ae02c79c3bc1
yosefalemu <yosefalemu007@gmail.com>
2024-10-15 11:55:31 +0300

    benefit service and collabration sections done

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
+import Benefits from "./components/Benefits";
+import Collaboration from "./components/Collaboration";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
+import Services from "./components/Services";
 
 function App() {
   return (
@@ -7,6 +10,9 @@ function App() {
       <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
         <Header />
         <Hero />
+        <Benefits />
+        <Collaboration />
+        <Services />
       </div>
     </>
   );
